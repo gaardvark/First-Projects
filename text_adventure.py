880a9ac011169dd4fcd26efa39ca098f84ca0235
Justin Reich <JustinReich@Justins-MacBook-Pro.local>
2015-02-23 20:31:29 -0600

    Initial commit

---
+++ text_adventure.py
@@ -0,0 +1,16 @@
+print '\nTHE PARK TRAIL OF DANGER!!!\n' 
+
+print '\nYou prepare to embark on a journey of horrors known as: THE PARK TRAIL OF DANGER!!!\n'
+print 'Many possible trials await ahead of you as you will ride a bike through a city park trail.' 
+print 'The following items are in your car: \n1) Pepper Spray \n2) Shoulder armor \n3) Iphone, 4-inch Knife \n4) Glock 9mm Pistol'
+print '\nYou may take one item with you.'
+item = raw_input('Which number do you choose?: ')
+print '\nOnly time will tell if you chose wisely...\n'
+
+print '\nTime slows as you feel the warm sun and hear the hot breeze wistle by as you race through a gorgous day at the speed up dreams...\n'
+print '\n"Nothing can harm me," you think to yourself; you are in your mind as invincible as the ancienct oak trees you pass by...\n'
+print '\nA sense of invulnerbility gives way to light contentment; as the minutes tick by, you are barely aware of your burning calves as they pump with the force of whirring pistons...' 
+
+
+
+
